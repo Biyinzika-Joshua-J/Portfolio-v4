bc16a2cac241b4773f4bda1688aa4c6027f6236c
Biyinyika-Joshua-J <joshuabiyinzika22@gmail.com>
2024-10-14 17:16:57 +0300

    fix build error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -51,20 +51,19 @@ export default function Home() {
           </div>
 
           <p className="text-lg mt-2">
-          Once upon a time, I used to think the "eat -{'>'} code -{'>'} sleep -{'>'} repeat" folks were nuts. But nowadays, that's all I do. I'll admit, it sometimes can soul wrenching.
-
-But the joy of building (and making) stuff with code ALWAYS out weighs the misery of writing code itself. #iykyk
-
-They say software engineering is all about trade offs, and you know what, they are right. Sometimes you trade your sanity for the love of solving some "complex code puzzles". It always pays off, at least it has for me.
-
-My name is Joshua Biyinzika, I'm a software engineer, I work with..... well, anything JavaScript: React, Next, Typescript and, wait - that's all.
-
-Then Python, Django, PostgreSQL and Docker.
-
-I'm also a wannabe solopreneur which has turned out to be a hard venture. Building software is one thing, selling it is another. Are you even in business if you don't make $$$ from your apps. Cause it's all about the Benjamins, isn't it?
-
-Inbox is always open... 
-
+            Once upon a time, I used to think the &quot;eat -{'>'} code -{'>'} sleep -{'>'}{' '}
+            repeat&quot; folks were nuts. But nowadays, that&apos;s all I do. I&apos;ll admit, it
+            sometimes can soul wrenching. But the joy of building (and making) stuff with code
+            ALWAYS out weighs the misery of writing code itself. #iykyk They say software
+            engineering is all about trade offs, and you know what, they are right. Sometimes you
+            trade your sanity for the love of solving some &quot;complex code puzzles&quot;. It
+            always pays off, at least it has for me. My name is Joshua Biyinzika, I&apos;m a
+            software engineer, I work with..... well, anything JavaScript: React, Next, Typescript
+            and, wait - that&apos;s all. Then Python, Django, PostgreSQL and Docker. I&apos;m also a
+            wannabe solopreneur which has turned out to be a hard venture. Building software is one
+            thing, selling it is another. Are you even in business if you don&apos;t make $$$ from
+            your apps. Cause it&apos;s all about the Benjamins, isn&apos;t it? Inbox is always
+            open...
           </p>
           <div className="flex items-center space-x-4 mt-6">
             {socialLinks.map((link) => (
